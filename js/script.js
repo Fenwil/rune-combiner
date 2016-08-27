9d26a5c340fa71a1cca2cc85d4148999c750c735
Leonardo Galante <fenwil@MacBook-Pro-de-Leonardo-2.local>
2016-08-27 17:50:57 -0300

    rune-combiner

---
+++ js/script.js
@@ -0,0 +1,311 @@
+var rune = [{
+  name: 'El',
+  image: '',
+  requiredLevel: 11,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Eld',
+  image: '',
+  requiredLevel: 11,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Tir',
+  image: '',
+  requiredLevel: 13,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Nef',
+  image: '',
+  requiredLevel: 13,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Eth',
+  image: '',
+  requiredLevel: 15,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ith',
+  image: '',
+  requiredLevel: 15,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Tal',
+  image: '',
+  requiredLevel: 17,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ral',
+  image: '',
+  requiredLevel: 19,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ort',
+  image: '',
+  requiredLevel: 21,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Thul',
+  image: '',
+  requiredLevel: 23,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Amn',
+  image: '',
+  requiredLevel: 25,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Sol',
+  image: '',
+  requiredLevel: 27,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Shael',
+  image: '',
+  requiredLevel: 29,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Dol',
+  image: '',
+  requiredLevel: 31,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Hel',
+  image: '',
+  requiredLevel: 33,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Io',
+  image: '',
+  requiredLevel: 35,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Lum',
+  image: '',
+  requiredLevel: 37,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ko',
+  image: '',
+  requiredLevel: 39,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Fal',
+  image: '',
+  requiredLevel: 41,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Lem',
+  image: '',
+  requiredLevel: 43,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Pul',
+  image: '',
+  requiredLevel: 45,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Um',
+  image: '',
+  requiredLevel: 47,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Mal',
+  image: '',
+  requiredLevel: 49,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ist',
+  image: '',
+  requiredLevel: 51,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Gul',
+  image: '',
+  requiredLevel: 53,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Vex',
+  image: '',
+  requiredLevel: 55,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ohm',
+  image: '',
+  requiredLevel: 57,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Lo',
+  image: '',
+  requiredLevel: 59,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Sur',
+  image: '',
+  requiredLevel: 61,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Ber',
+  image: '',
+  requiredLevel: 63,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Jah',
+  image: '',
+  requiredLevel: 65,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Cham',
+  image: '',
+  requiredLevel: 67,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}, {
+  name: 'Zod',
+  image: '',
+  requiredLevel: 69,
+  properties: {
+    weapon: '',
+    armorHelm: '',
+    shield: '',
+  }
+}]
+
+new Vue({
+  el: '#app',
+
+  data: {
+    search: '',
+    runes: rune
+  },
+
+  methods: {
+
+  }
+})
