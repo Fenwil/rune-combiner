d7d494c6775e5f6699b19c438904a8bf5e8dfe19
Leonardo Galante <fenwil@MacBook-Pro-de-Leonardo-2.local>
2016-08-27 20:15:46 -0300

    bootstrap not conviced

--- js/script.js
+++ js/script.js
@@ -3,188 +3,188 @@ var rune = [{
   image: '',
   requiredLevel: 11,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+50 To Attack Rating, +1 Light Radius',
+    armorHelm: '+15 Defense, +1 To Light Radius',
+    shield: '+15 Defense, +1 To Light Radius',
   }
 }, {
   name: 'Eld',
   image: '',
   requiredLevel: 11,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+75% Damage To Undead, +50 Attack Rating Against Undead',
+    armorHelm: '+15% Slower Stamina Drain',
+    shield: '7% Increased Chance of Blocking',
   }
 }, {
   name: 'Tir',
   image: '',
   requiredLevel: 13,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+2 To Mana After Each Kill',
+    armorHelm: '+2 To Mana After Each Kill',
+    shield: '+2 To Mana After Each Kill',
   }
 }, {
   name: 'Nef',
   image: '',
   requiredLevel: 13,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: 'Knockback',
+    armorHelm: '+30 Defense Vs. Missile',
+    shield: '+30 Defense Vs. Missile',
   }
 }, {
   name: 'Eth',
   image: '',
   requiredLevel: 15,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '-25% To Target Defense',
+    armorHelm: 'Regenerate Mana 15%',
+    shield: 'Regenerate Mana 15%',
   }
 }, {
   name: 'Ith',
   image: '',
   requiredLevel: 15,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+9 To Maximum Damage',
+    armorHelm: '15% Damage Taken Goes To Mana',
+    shield: '15% Damage Taken Goes To Mana',
   }
 }, {
   name: 'Tal',
   image: '',
   requiredLevel: 17,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+75 Poison Damage Over 5 Seconds',
+    armorHelm: 'Poison Resist 30%',
+    shield: 'Poison Resist 35%',
   }
 }, {
   name: 'Ral',
   image: '',
   requiredLevel: 19,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: 'Adds 5-30 Fire Damage',
+    armorHelm: 'Fire Resist 30%',
+    shield: 'Fire Resist 35%',
   }
 }, {
   name: 'Ort',
   image: '',
   requiredLevel: 21,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: 'Adds 1-50 Lightning Damage',
+    armorHelm: 'Lightning Resist 30%',
+    shield: 'Lightning Resist 35%',
   }
 }, {
   name: 'Thul',
   image: '',
   requiredLevel: 23,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: 'Adds 3-14 Cold Damage - 3 Second Duration',
+    armorHelm: 'Cold Resist 30%',
+    shield: 'Cold Resist 35%',
   }
 }, {
   name: 'Amn',
   image: '',
   requiredLevel: 25,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '7% Life Stolen Per Hit',
+    armorHelm: 'Attacker Takes Damage of 14',
+    shield: 'Attacker Takes Damage of 14',
   }
 }, {
   name: 'Sol',
   image: '',
   requiredLevel: 27,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+9 To Minimum Damage',
+    armorHelm: 'Damage Reduced by 7',
+    shield: 'Damage Reduced by 7',
   }
 }, {
   name: 'Shael',
   image: '',
   requiredLevel: 29,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '20% Increased Attack Speed',
+    armorHelm: '20% Faster Hit Recovery',
+    shield: '20% Faster Block Rate',
   }
 }, {
   name: 'Dol',
   image: '',
   requiredLevel: 31,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: 'Hit Causes Monster To Flee 25%',
+    armorHelm: 'Replenish Life +7',
+    shield: 'Replenish Life +7',
   }
 }, {
   name: 'Hel',
   image: '',
   requiredLevel: 33,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: 'Requirements -20%',
+    armorHelm: 'Requirements -15%',
+    shield: 'Requirements -15%',
   }
 }, {
   name: 'Io',
   image: '',
   requiredLevel: 35,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+10 To Vitality',
+    armorHelm: '+10 To Vitality',
+    shield: '+10 To Vitality',
   }
 }, {
   name: 'Lum',
   image: '',
   requiredLevel: 37,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+10 To Energy',
+    armorHelm: '+10 To Energy',
+    shield: '+10 To Energy',
   }
 }, {
   name: 'Ko',
   image: '',
   requiredLevel: 39,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+10 To Dexterity',
+    armorHelm: '+10 To Dexterity',
+    shield: '+10 To Dexterity',
   }
 }, {
   name: 'Fal',
   image: '',
   requiredLevel: 41,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '+10 To Strength',
+    armorHelm: '+10 To Strength',
+    shield: '+10 To Strength',
   }
 }, {
   name: 'Lem',
   image: '',
   requiredLevel: 43,
   properties: {
-    weapon: '',
-    armorHelm: '',
-    shield: '',
+    weapon: '75% Extra Gold From Monsters',
+    armorHelm: '50% Extra Gold From Monsters',
+    shield: '50% Extra Gold From Monsters',
   }
 }, {
   name: 'Pul',
   image: '',
   requiredLevel: 45,
   properties: {
-    weapon: '',
-    armorHelm: '',
+    weapon: '+75% Damage to Demons, +100 Attack Rating Against Demons',
+    armorHelm: '+30% Enhanced Defense',
     shield: '',
   }
 }, {
